94c1cc71292f51f41175c89cbc94a8b5a002c97a
Jason Permann <JaceFoxman@gmail.com>
2025-02-15 11:36:15 -0700

    Add first conversion function

--- ConvertAndValidate/ConvertAndValidate.vb
+++ ConvertAndValidate/ConvertAndValidate.vb
@@ -13,17 +13,32 @@ Module ConvertAndValidate
         'testing
         Dim aValidNumber As Integer
         Dim usernResponse As String
-        Dim ConversionValid As Boolean = False
+
         Do
             Console.WriteLine($"Enter a number:")
             usernResponse = Console.ReadLine()
 
             If ConversionValid(usernResponse, aValidNumber) = True Then
-                Console.WriteLine($"{UsernResponse} converted successfully to {aValidNumber}!")
+                Console.WriteLine($"{usernResponse} converted successfully to {aValidNumber}!")
             Else
                 Console.WriteLine($"Oops,{usernResponse} does not seem to be a number")
             End If
         Loop
     End Sub
+
+    Function ConversionValid(convertThisString As String, toThisInteger As Integer) As Boolean
+
+        Dim status As Boolean = False
+
+        Try
+            toThisInteger = CInt(convertThisString)
+            status = True
+        Catch ex As Exception
+
+        End Try
+
+        Return status
+
+    End Function
 
 End Module

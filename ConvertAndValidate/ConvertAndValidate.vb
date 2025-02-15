863a950573c40e70b75789752bc5fbe24c0bd07b
Jason Permann <JaceFoxman@gmail.com>
2025-02-15 11:16:57 -0700

    Add testing code

--- ConvertAndValidate/ConvertAndValidate.vb
+++ ConvertAndValidate/ConvertAndValidate.vb
@@ -3,10 +3,27 @@
 'RCET2265
 'Convert And Validate
 'https://github.com/JaceFoxman/ConvertAndValidate.git
+
+Option Explicit On
+Option Strict On
+Option Compare Text
 Module ConvertAndValidate
 
     Sub Main()
+        'testing
+        Dim aValidNumber As Integer
+        Dim usernResponse As String
+        Dim ConversionValid As Boolean = False
+        Do
+            Console.WriteLine($"Enter a number:")
+            usernResponse = Console.ReadLine()
 
+            If ConversionValid(usernResponse, aValidNumber) = True Then
+                Console.WriteLine($"{UsernResponse} converted successfully to {aValidNumber}!")
+            Else
+                Console.WriteLine($"Oops,{usernResponse} does not seem to be a number")
+            End If
+        Loop
     End Sub
 
 End Module
